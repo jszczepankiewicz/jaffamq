e37abb85b0025728dec62e954f0b19dff3c49856
jszczepankiewicz <jszczepankiewicz@gmail.com>
2013-12-15 14:23:32 +0100

    Minor javadoc. Unused obsolete code.

--- spearfish-db/src/main/java/org/jaffamq/persistence/PersistedMessageId.java
+++ spearfish-db/src/main/java/org/jaffamq/persistence/PersistedMessageId.java
@@ -1,7 +1,8 @@
 package org.jaffamq.persistence;
 
 /**
- * Interface for unique identifier of message that can be persisted. Real methods depends on the
+ * Interface for unique identifier of message that can be persisted. It is more like marker interface because depending on the
+ * physical persistence layer implementing classes will expose implementation specific identifiers.
  */
 public interface PersistedMessageId {
 }
