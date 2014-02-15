e0f2b0f4baa47da6500f9d9d8516dbb504072afa
jszczepankiewicz <jszczepankiewicz@gmail.com>
2014-02-15 18:00:26 +0100

    Added GroupRepository

---
+++ spearfish-db/src/main/java/org/jaffamq/persistence/database/repository/GroupRepository.java
@@ -0,0 +1,8 @@
+package org.jaffamq.persistence.database.repository;
+
+/**
+ * Created by urwisy on 19.01.14.
+ */
+public class GroupRepository {
+
+}
---
+++ spearfish-db/src/test/java/org/jaffamq/persistence/database/repository/GroupRepositoryTest.java
@@ -0,0 +1,29 @@
+package org.jaffamq.persistence.database.repository;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Created by urwisy on 19.01.14.
+ */
+public class GroupRepositoryTest {
+
+    @Ignore
+    @Test
+    public void shouldDeleteGroup(){
+
+    }
+
+    @Ignore
+    @Test
+    public void shouldDeleteGroupCascadeForUsers(){
+
+    }
+
+    @Ignore
+    @Test
+    public void shouldAddGroup(){
+
+    }
+
+}
